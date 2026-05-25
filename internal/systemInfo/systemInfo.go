b5deff553fcb925051de968d8dcf9e281b6f6733
dishan1223 <dishanishtiaq45+github@gmail.com>
2026-05-25 17:08:27 +0600

    fix timeline issue

--- internal/systemInfo/systemInfo.go
+++ internal/systemInfo/systemInfo.go
@@ -13,16 +13,14 @@ import (
 
 // trying to make the intent detection system better with this function. this way
 // we dont need to write a big condition in each if statements
-func containsAny(msg string, keywords []string) bool{
-    for _ , k := range keywords {
-        if strings.Contains(msg, k){
-            return true
-        }
-    }
-    return false
+func containsAny(msg string, keywords []string) bool {
+	for _, k := range keywords {
+		if strings.Contains(msg, k) {
+			return true
+		}
+	}
+	return false
 }
-
-
 
 // this functions provides extra dependencies to the AI model
 // 1. Weather data
@@ -30,69 +28,76 @@ func containsAny(msg string, keywords []string) bool{
 // this shall be passed asa a context to the AI
 // basically a library of data for the ai model to read to provide better response
 // maybe on kind of training
-func GetModelDeps(msg string, lat string, lon string) (string, error){
+func GetModelDeps(msg string, lat string, lon string) (string, error) {
 
-    msg = strings.ToLower(msg)
+	msg = strings.ToLower(msg)
 
-    var system_info string
-    
-    // Time&Date intent check
-    if containsAny(msg, consts.TIME_KEYWORDS){
-        system_info = time.Now().Format(time.RFC3339)
-    }
-    
+	var system_info string
 
-    // Weather Intent Check
-    if containsAny(msg, consts.WEATHER_KEYWORDS){
+	// Time&Date intent check
+	if containsAny(msg, consts.TIME_KEYWORDS) {
+		// when i hosted the server on Render, the model was not responding bangladeshi time
+		// LoadLocation("asia/dhaka") sets the location to bangladesh and then we get accurate data
+		// for Bangladeshi time
 
-        w,err := api.GetWeatherReport(lat, lon)
+		loc, err := time.LoadLocation("Asia/Dhaka")
+		if err != nil {
+			return system_info, err
+		}
 
-        if err != nil{
-            fmt.Println("Error getting weather data: ",err)
-            system_info = ""
-            // loggin the error in this function. so maybe i dont need to send the error to 
-            // the controller
-            return system_info, err 
-        }
-        weatherDataforAiModel := fmt.Sprintf(
-        	`Weather Report:
+		bangladeshiTime := time.Now().In(loc)
+
+		system_info = bangladeshiTime.Format(time.RFC3339)
+	}
+
+	// Weather Intent Check
+	if containsAny(msg, consts.WEATHER_KEYWORDS) {
+
+		w, err := api.GetWeatherReport(lat, lon)
+
+		if err != nil {
+			fmt.Println("Error getting weather data: ", err)
+			system_info = ""
+			// loggin the error in this function. so maybe i dont need to send the error to
+			// the controller
+			return system_info, err
+		}
+		weatherDataforAiModel := fmt.Sprintf(
+			`Weather Report:
             Location: %s, %s
             Condition: %s
             Temperature: %.1f°C
             Feels Like: %.1f°C
             Humidity: %d%%
             Wind Speed: %.1f m/s`,
-	        w.Location,
-	        w.Country,
-	        w.Description,
-	        w.Temp,
-	        w.FeelsLike,
-	        w.Humidity,
-	        w.WindSpeed,
-        )
-        system_info +="\nWeather Data Context " + weatherDataforAiModel
-    }
+			w.Location,
+			w.Country,
+			w.Description,
+			w.Temp,
+			w.FeelsLike,
+			w.Humidity,
+			w.WindSpeed,
+		)
+		system_info += "\nWeather Data Context " + weatherDataforAiModel
+	}
 
+	if containsAny(msg, consts.SEARCH_KEYWORDS) {
+		searchResults, err := service.GetWebResultsFromLangSearch(msg)
+		if err != nil {
+			fmt.Println("Error from search service")
 
-    if containsAny(msg, consts.SEARCH_KEYWORDS){
-        searchResults, err := service.GetWebResultsFromLangSearch(msg)
-        if err != nil{
-            fmt.Println("Error from search service")
+			return system_info, err
+		}
 
-            return system_info, err
-        }
+		searchDataToAI, err := sonic.MarshalString(searchResults)
+		if err != nil {
+			fmt.Println("Error marshaling search results:", err)
+			return system_info, err
+		}
 
-        searchDataToAI , err := sonic.MarshalString(searchResults) 
-        if err != nil{
-            fmt.Println("Error marshaling search results:", err)
-            return system_info, err
-        }
+		system_info += "\nSearch Context : " + searchDataToAI
+	}
 
-        system_info += "\nSearch Context : " + searchDataToAI
-    }
-
-    return system_info,nil
+	return system_info, nil
 
 }
-
-
